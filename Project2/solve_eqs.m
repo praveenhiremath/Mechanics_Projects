307439a2e5bf5e6fae1cf5c8c4e91863d3795ff4
praveenhiremath <44702459+praveenhiremath@users.noreply.github.com>
2024-01-09 15:56:12 +0100

    Create solve_eqs.m
    Auxiliary function file

---
+++ Project2/solve_eqs.m
@@ -0,0 +1,20 @@
+function [M, N_A, N_B] = solve_eqs(mu_s, theta)
+% so we have a 3 unknowns and 3 equations
+theta = deg2rad(theta);
+M = zeros(length(mu_s), length(theta));
+N_A = zeros(length(mu_s), length(theta));
+N_B = zeros(length(mu_s), length(theta));
+
+for j = 1:length(mu_s)
+    for i = 1:length(theta)
+        matrix = [1 -0.2*mu_s(j) -mu_s(j)*0.2; 0 -1 sin(theta(i))+mu_s(j)*cos(theta(i)); 0 mu_s(j) cos(theta(i))-mu_s(j)*sin(theta(i))];
+        % X = [M N_A N_B];
+        X = matrix \ [0 ; 0 ; 30*9.81];
+        inv(matrix);
+        X2 = inv(matrix)*[0 ; 0 ; 30*9.81];
+        M(j,i) = X(1);
+        N_A(j,i) = X(2);
+        N_B(j,i) = X(3);
+    end
+end
+end
