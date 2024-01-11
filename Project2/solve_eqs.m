d9d054e9ee3c6edb859510bbd6f80445453e0d64
praveenhiremath <44702459+praveenhiremath@users.noreply.github.com>
2024-01-11 10:56:46 +0100

    Update solve_eqs.m

--- Project2/solve_eqs.m
+++ Project2/solve_eqs.m
@@ -1,5 +1,6 @@
 function [M, N_A, N_B] = solve_eqs(mu_s, theta)
 % This code was developed for teaching purposes at Lund University
+% This code is not to be used by others for University submission.
 % Author: Praveenkumar Hiremath
 % Email: praveenkumar.hiremath@mek.lth.se (Email at the University)
 %       praveenkumar.hiremath2911@gmail.com (Private email)
