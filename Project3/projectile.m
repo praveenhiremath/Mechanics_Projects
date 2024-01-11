c6be0b60e2c63bec1f3727d57753055ac3740f53
praveenhiremath <44702459+praveenhiremath@users.noreply.github.com>
2024-01-11 10:53:12 +0100

    Update projectile.m

--- Project3/projectile.m
+++ Project3/projectile.m
@@ -1,4 +1,9 @@
 %% Exercise 3
+% This code was developed for teaching purposes at Lund University
+% Author: Praveenkumar Hiremath
+% Email: praveenkumar.hiremath@mek.lth.se (Email at the University)
+%       praveenkumar.hiremath2911@gmail.com (Private email)
+
 clc
 clear all
 
